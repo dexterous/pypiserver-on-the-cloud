e98ec1c2dff2789e36269b51d2bae47daad6abc9
Saager Mhatre <saager.mhatre@gmail.com>
2012-08-22 18:43:14 +0530

    Remove bottle app script
    just use bottle runner embedded in pypiserver

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bottle_app.py
+web: python -m pypiserver.bottle --bind="0.0.0.0:${PORT-5000}" wsgi:application
--- bottle_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import wsgi
-import pypiserver.bottle as bottle
-
-bottle.run(
-    app=wsgi.application,
-    host='0.0.0.0',
-    port=int(os.environ.get('PORT', 5000))
-)
